0e36cb8860c94a8cc74f12098dbe5670c76cf216
anishas07 <Studytea95@gmail.com>
2024-12-18 18:17:16 +1030

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function click1()
+{
+    //javascript first statements. 
+    document.getElementById("p1").innerHTML = " Hello This is javascript" ;
+    document.getElementById("p2").innerHTML = "Javascript allows a website to be functional and directly take you there "
+}
